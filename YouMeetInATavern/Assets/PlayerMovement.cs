bf045a977116994223827744a67df4a3cb3d21ce
Anthony <messinabrothers@gmail.com>
2018-05-22 09:23:03 -0700

    Player model, movement, and rotation

---
+++ YouMeetInATavern/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed, rotSpeed;
+
+    private Quaternion lookQuat;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        // move player
+        float h = moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float v = moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.Translate(h, 0, v, Space.World);
+
+        // slowly took at forward direction
+        if (h != 0 || v != 0) {
+            lookQuat = Quaternion.LookRotation(new Vector3(h, 0, v));
+        }
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookQuat, rotSpeed * Time.deltaTime);
+        
+    }
+}
