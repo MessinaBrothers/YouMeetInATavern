f27cc5fdef75a76e43d26f88475a31ec8ca1992d
Anthony <messinabrothers@gmail.com>
2018-05-22 09:52:18 -0700

    Enemy Minion: model, follows player, looks at player

---
+++ YouMeetInATavern/Assets/AttackPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour {
+
+    public float reachDistance, rotSpeed;
+
+    private SteeringBehavior steering;
+
+    private Transform player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        steering = GetComponent<SteeringBehavior>();
+    }
+
+    void Update() {
+        // if distance to player is greater that reach distance
+        if ((transform.position - player.position).sqrMagnitude > reachDistance * reachDistance) {
+            // set the steering behaviour target
+            steering.target = player;
+        } else {
+            steering.target = null;
+        }
+
+        //slowly look at player
+        Quaternion lookQuat = Quaternion.LookRotation(player.position - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookQuat, rotSpeed * Time.deltaTime);
+    }
+}
---
+++ YouMeetInATavern/Assets/SteeringBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringBehavior : MonoBehaviour {
+
+    public float moveSpeed;
+
+    public Transform target;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        if (target != null) {
+            // move to target
+            transform.Translate((target.position - transform.position).normalized * moveSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
