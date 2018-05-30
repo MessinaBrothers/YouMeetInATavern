804139b6c20d6306c74d50bc09ca41cb9f7f3125
Anthony <messinabrothers@gmail.com>
2018-05-30 10:39:33 -0700

    Health: disappears when dead

--- YouMeetInATavern/Assets/Health.cs
+++ YouMeetInATavern/Assets/Health.cs
@@ -44,6 +44,7 @@ public class Health : MonoBehaviour {
 
             if (current <= 0) {
                 deathEventHandler.Invoke(id);
+                Destroy(gameObject);
             }
         }
     }
