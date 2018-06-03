033372565cb917e91a6f28f8824eb8c46edb27a2
Anthony <messinabrothers@gmail.com>
2018-06-03 09:23:14 -0700

    Reorganized top-level folders

---
+++ YouMeetInATavern/Assets/LoadController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadController : MonoBehaviour {
+
+    public GameObject startingWeaponPrefab;
+    
+	void Start () {
+        // load the player's weapon icon on game start
+        // in the future: equip the weapon for the player?
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        
+	}
+	
+	void Update () {
+		
+	}
+}
