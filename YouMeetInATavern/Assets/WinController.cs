b79b907ac6ce518be719e00a3c377a3c9339ca06
Anthony <messinabrothers@gmail.com>
2018-06-03 07:35:20 -0700

    Lose screen triggers when player health = 0

--- YouMeetInATavern/Assets/WinController.cs
+++ YouMeetInATavern/Assets/WinController.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinController : MonoBehaviour {
 
-    void Start() {
+    private int playerID;
 
+    void Start() {
+        playerID = GameObject.FindGameObjectWithTag("Player").GetComponent<EntityID>().id;
     }
 
     void Update() {
 
+    }
+
+    private void CheckDeath(int id) {
+        if (id == playerID) {
+            SceneManager.LoadScene("LoseScreen");
+        }
+    }
+
+    void OnEnable() {
+        Health.deathEventHandler += CheckDeath;
+    }
+
+    void OnDisable() {
+        Health.deathEventHandler -= CheckDeath;
     }
 }
