66316a925484892afebec8d1a8b503c0086b78c4
Anthony <messinabrothers@gmail.com>
2018-06-14 09:16:14 -0700

    Dialogue: Fixed bug when NPC text contained a comma. Added barkeep dialogue

--- YouMeetInATavern/Assets/DialogueController.cs
+++ YouMeetInATavern/Assets/DialogueController.cs
@@ -18,18 +18,22 @@ public class DialogueController : DataUser {
         string[] textLines = textFile.text.Split('\n');
         for (int i = 1; i < textLines.Length - 1; i++) {
             string[] textData = textLines[i].Split(',');
+            //print(textLines[i]);
+            //for (int j = 0; j < textData.Length; j++) print(textData[j]);
+
             int index = 0;
             uint id = uint.Parse(textData[index++]);
             Dialogue.Type type = textData[index++].Equals("Prompt") ? Dialogue.Type.PROMPT : Dialogue.Type.CHOICE;
             string text = textData[index++];
             // account for commas in text dialogue e.g. "Guards, help!"
-            if (textData[index].Contains("\"")) text += textData[index++];
-            uint next0 = textData[index++].Length == 0 ? GameData.INVALID_UID : uint.Parse(textData[3]);
-            uint next1 = textData[index++].Length == 0 ? GameData.INVALID_UID : uint.Parse(textData[4]);
-            uint next2 = textData[index++].Length == 0 ? GameData.INVALID_UID : uint.Parse(textData[5]);
+            if (textData[index].Contains("\"")) text += ',' + textData[index++];
+            uint next0 = textData[index].Length == 0 ? GameData.INVALID_UID : uint.Parse(textData[index++]);
+            uint next1 = textData[index].Length == 0 ? GameData.INVALID_UID : uint.Parse(textData[index++]);
+            uint next2 = textData[index].Length == 0 ? GameData.INVALID_UID : uint.Parse(textData[index++]);
             string reward = textData[index++];
 
             Dialogue dialogue = new Dialogue(id, type, text, next0, next1, next2, reward);
+            print(dialogue);
             data.dialogues.Add(id, dialogue);
         }
     }
