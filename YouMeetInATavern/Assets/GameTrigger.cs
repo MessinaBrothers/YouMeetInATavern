50683ee757a64839ea20595118a125ff315b8e13
Anthony <messinabrothers@gmail.com>
2018-06-20 09:28:56 -0700

    Enemy: Waves appear after player attempts to leave the bar

---
+++ YouMeetInATavern/Assets/GameTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTrigger : MonoBehaviour {
+
+    public GameObject[] toEnable;
+
+    void OnEnable() {
+        TavernDoor.leaveBarEventHandler += LeaveTavern;
+    }
+
+    void OnDisable() {
+        TavernDoor.leaveBarEventHandler -= LeaveTavern;
+    }
+
+    private void LeaveTavern() {
+        foreach (GameObject go in toEnable) {
+            go.SetActive(true);
+        }
+    }
+}
---
+++ YouMeetInATavern/Assets/TavernDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavernDoor : MonoBehaviour {
+
+    public static event LeaveBarEventHandler leaveBarEventHandler;
+    public delegate void LeaveBarEventHandler();
+
+    private GameObject player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (player == other.gameObject) {
+            leaveBarEventHandler.Invoke();
+        }
+    }
+}
