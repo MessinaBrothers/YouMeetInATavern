203234ca9d24f6360fe5a1b430b49fd0e35b9bc9
Anthony <messinabrothers@gmail.com>
2018-07-07 17:10:40 -0700

    Card object moves into introduction position

---
+++ YouMeetInATavern/Assets/Scripts/IntroduceNPC.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroduceNPC : MonoBehaviour {
+
+    public GameObject cardPrefab;
+
+    public Transform offscreenPos, introPos;
+
+    public bool startIntro;
+
+    public float moveIntroTime;
+    private float moveIntroTimer;
+
+    private GameObject card;
+
+    void Start() {
+        card = Instantiate(cardPrefab);
+
+        card.transform.position = offscreenPos.position;
+        card.transform.rotation = offscreenPos.rotation;
+    }
+
+    void Update() {
+        if (startIntro == true) {
+            startIntro = false;
+
+            moveIntroTimer = moveIntroTime;
+        }
+
+        if (moveIntroTimer > 0) {
+            moveIntroTimer -= Time.deltaTime;
+            card.transform.position = Vector3.Slerp(introPos.position, offscreenPos.position, moveIntroTimer / moveIntroTime);
+        }
+    }
+}
