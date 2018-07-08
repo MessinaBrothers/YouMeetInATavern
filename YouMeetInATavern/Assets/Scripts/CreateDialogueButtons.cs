d3fe9f1b553ac6e919caac54ff5ddfe8e99caefb
Anthony <messinabrothers@gmail.com>
2018-07-08 09:45:04 -0700

    Dialogue prevents whole words from overflowing

--- YouMeetInATavern/Assets/Scripts/CreateDialogueButtons.cs
+++ YouMeetInATavern/Assets/Scripts/CreateDialogueButtons.cs
@@ -7,89 +7,88 @@ public class CreateDialogueButtons : MonoBehaviour {
 
     public GameObject buttonPrefab;
 
-    public string dialogue = "<>Sit anywhere you like.<>The tavern's dead right now.";
+    public string dialogue;
 
     public float marginOfError;
 
-    private GameObject lastButton;
-    private Text lastButtonText;
+    private GameObject currentButton;
+    private Text currentButtonText;
 
     private string[] wordsToAdd;
     private int wordsToAddIndex;
 
-    private string wordsSoFar;
-
     private float widthSum; // track button widths
 
+    private int charIndex;
+    private string wordsSoFar, currentWord;
+
     void Start() {
-        CreateButtons(dialogue);
-    }
+        print(dialogue);
 
-    void Update() {
-        //// add words, one at a time
-        //if (wordsToAddIndex < wordsToAdd.Length) {
-        //    lastButtonText.text += " " + wordsToAdd[wordsToAddIndex];
-        //    wordsToAddIndex += 1;
-        //    LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
-
-        //    // determine button width
-        //    Rect lastRect = lastButton.GetComponent<RectTransform>().rect;
-        //    float width = GetComponent<RectTransform>().rect.width;
-        //    print(width);
-        //    if (widthSum + lastRect.width + marginOfError > width) {
-        //        print("Button is too big!");
-        //        wordsToAddIndex = int.MaxValue;
-        //        lastButtonText.text = wordsSoFar;
-        //        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
-        //    } else {
-        //        print("Button fits");
-        //        wordsSoFar = lastButtonText.text;
-        //    }
-        //}
-    }
-
-    void OnGUI() {
-        // add words, one at a time
-        if (wordsToAddIndex < wordsToAdd.Length) {
-            lastButtonText.text += " " + wordsToAdd[wordsToAddIndex];
-            wordsToAddIndex += 1;
-            LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
-
-            // determine button width
-            Rect lastRect = lastButton.GetComponent<RectTransform>().rect;
-            float width = GetComponent<RectTransform>().rect.width;
-            print(width);
-            if (widthSum + lastRect.width + marginOfError > width) {
-                print("Button is too big!");
-                wordsToAddIndex = int.MaxValue;
-                lastButtonText.text = wordsSoFar;
-                LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
-            } else {
-                print("Button fits");
-                wordsSoFar = lastButtonText.text;
-            }
+        // if the dialogue lacks an index at the start, append one
+        if (dialogue[0] != '<') {
+            dialogue = "<>" + dialogue;
         }
     }
 
-    private void CreateButtons(string dialogue) {
-        CreateButton("Sit anywhere you like. Words will be added to make the line too long.");
-        //CreateButton("Tavern's dead right now.");
+    void Update() {
+        if (charIndex < dialogue.Length) {
+            char c = dialogue[charIndex];
+            //Debug.LogFormat("Parsing char {0} at index {1}", c, charIndex);
+            switch (c) {
+                case '<':
+                    // parse the key
+                    int endIndex = dialogue.IndexOf('>', charIndex);
+                    int key = endIndex - charIndex > 1 ? int.Parse(dialogue.Substring(charIndex + 1, endIndex - charIndex - 1)) : 0;
+                    Debug.LogFormat("Found key {0} at index {1}", key, charIndex);
+                    charIndex = endIndex;
+
+                    // save the last button width
+                    if (currentButton != null) {
+                        widthSum += currentButton.GetComponent<RectTransform>().rect.width;
+                    }
+
+                    // clear the current words
+                    wordsSoFar = "";
+                    currentWord = "";
+
+                    // start a new button
+                    CreateButton(key);
+                    break;
+                default:
+                    // if we encounter a space, append the last word to words so far
+                    if (c == ' ') {
+                        wordsSoFar += " " + currentWord;
+                        wordsSoFar = wordsSoFar.Trim();
+                        print(currentWord);
+                        currentWord = "";
+                    } else {
+                        currentWord += c;
+                        currentButtonText.text = wordsSoFar + " " + currentWord;
+                        LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+
+                        // determing if button is too big
+                        float lastWidth = currentButton.GetComponent<RectTransform>().rect.width;
+                        float width = GetComponent<RectTransform>().rect.width;
+                        if (widthSum + lastWidth + marginOfError > width) {
+                            charIndex = dialogue.Length;
+                            // set the button text, ignoring the current word
+                            currentButtonText.text = wordsSoFar;
+                            LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+
+                            print(wordsSoFar);
+                        }
+                    }
+                    break;
+            }
+            charIndex += 1;
+        }
     }
 
-    private void CreateButton(string snippet) {
-        wordsToAdd = snippet.Split(' ');
-        wordsToAddIndex = 0;
-
-        wordsSoFar = "";
-
-        lastButton = Instantiate(buttonPrefab);
-        lastButtonText = lastButton.GetComponentInChildren<Text>();
-        lastButtonText.text = "";
-        lastButton.transform.SetParent(transform, false);
-
-        // TODO determine if button is too big
-        // If too big, cut a word and retry
-        // Keep doing until no words left
-        // Create a new Panel with the remaining dialogue
+    private void CreateButton(int key) {
+        currentButton = Instantiate(buttonPrefab);
+        currentButtonText = currentButton.GetComponentInChildren<Text>();
+        currentButtonText.text = "";
+        currentButton.transform.SetParent(transform, false);
     }
 }
