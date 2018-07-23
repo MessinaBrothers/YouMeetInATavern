7a587d1e3c6c2c048d6cda9900086b8ea432abd4
Anthony <messinabrothers@gmail.com>
2018-07-23 07:59:30 -0700

    Clicking on UI no longer clicks on any underlying cards

--- YouMeetInATavern/Assets/Scripts/CardClickable.cs
+++ YouMeetInATavern/Assets/Scripts/CardClickable.cs
@@ -7,7 +7,11 @@ public class CardClickable : MonoBehaviour {
     public static event CardClickedEventHandler cardClickedEventHandler;
     public delegate void CardClickedEventHandler(GameObject card);
 
-    void OnMouseDown() {
+    //void OnMouseDown() {
+    //    cardClickedEventHandler(gameObject);
+    //}
+
+    public void Broadcast() {
         cardClickedEventHandler(gameObject);
     }
 }
