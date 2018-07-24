e3ffa5fd709c08db5744e09a53abdff2c0f72e45
Anthony <messinabrothers@gmail.com>
2018-07-24 10:57:57 -0700

    When the user clicks on an item card, the card zooms forward

---
+++ YouMeetInATavern/Assets/Scripts/CardZoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardZoom : MonoBehaviour {
+
+    public GameObject defaultPos, zoomPos;
+
+    private float timer, time;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    public void Zoom() {
+        gameObject.transform.position = zoomPos.transform.position;
+    }
+    
+    public void Unzoom() {
+
+    }
+}
