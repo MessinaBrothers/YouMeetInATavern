451c7535efb80bcb909a5c335217cced037d92c4
Anthony <messinabrothers@gmail.com>
2018-08-26 15:32:59 -0700

    Profiler: Time loading the game

---
+++ YouMeetInATavern/Assets/DEBUGProfiler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUGProfiler : MonoBehaviour {
+
+    private DateTime startTime;
+
+    void Awake() {
+        startTime = DateTime.Now;
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    public void EndLoadProfiler() {
+        Debug.LogFormat("Game load time: {0:0.} ms", (DateTime.Now - startTime).TotalMilliseconds);
+    }
+}
---
+++ YouMeetInATavern/Assets/DEBUGProfilerEnd.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEBUGProfilerEnd : MonoBehaviour {
+    
+    void Update() {
+        GetComponent<DEBUGProfiler>().EndLoadProfiler();
+        enabled = false;
+    }
+}
