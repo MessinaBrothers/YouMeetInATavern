10a73561f1e71805585be56a24088aed79b91424
Anthony <messinabrothers@gmail.com>
2018-09-16 13:30:37 -0700

    Hex collider rescales when rising and lowering

--- YouMeetInATavern/Assets/Scripts/GUI/HexagonCollider.cs
+++ YouMeetInATavern/Assets/Scripts/GUI/HexagonCollider.cs
@@ -6,11 +6,34 @@ public class HexagonCollider : MonoBehaviour {
 
     public int id;
 
+    public float lowerPosition, risePosition;
+    public float lowerScale, riseScale;
+
+    private Vector3 position;
+    private Vector3 scale;
+
+    void Start() {
+        position = transform.localPosition;
+        scale = transform.localScale;
+    }
+
     void OnMouseEnter() {
         InputController.HoverOverHex(id);
+        
+        Set(risePosition, riseScale);
     }
 
     void OnMouseExit() {
         InputController.HoverExitHex(id);
+
+        Set(lowerPosition, lowerScale);
+    }
+
+    void Set(float positionY, float scaleZ) {
+        position.y = positionY;
+        transform.localPosition = position;
+
+        scale.z = scaleZ;
+        transform.localScale = scale;
     }
 }
