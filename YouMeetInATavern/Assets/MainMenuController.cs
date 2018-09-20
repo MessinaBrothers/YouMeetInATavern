9f63155c4c8239ea4083a8220375f7085dab9f40
Anthony <messinabrothers@gmail.com>
2018-09-20 09:10:22 -0700

    Main Menu: once faded out, a loading screen appears and the next scene is loaded

--- YouMeetInATavern/Assets/MainMenuController.cs
+++ YouMeetInATavern/Assets/MainMenuController.cs
@@ -2,28 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
 
-    public float fadeOutTime;
+    public GameObject loadScreen;
     public FadeImage fadeImage;
+    public float fadeOutTime;
 
     private MusicController musicController;
 
     void Start() {
         musicController = FindObjectOfType<MusicController>();
+
+        loadScreen.SetActive(false);
     }
     
     void OnEnable() {
         InputController.startGameEventHandler += FadeOut;
+        InputController.fadedOutEventHandler += StartGame;
     }
 
     void OnDisable() {
         InputController.startGameEventHandler -= FadeOut;
+        InputController.fadedOutEventHandler -= StartGame;
     }
 
     private void FadeOut() {
         fadeImage.FadeOut(fadeOutTime);
         musicController.TransitionSilent(fadeOutTime);
+    }
+
+    private void StartGame() {
+        loadScreen.SetActive(true);
+        SceneManager.LoadScene("Main");
     }
 }
