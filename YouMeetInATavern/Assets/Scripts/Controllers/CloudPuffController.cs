760b1ed5ad60d9ba4a2d7615dc6bdf80e1886ec3
Anthony <messinabrothers@gmail.com>
2018-11-07 08:08:05 -0800

    Added puff clouds when clicking on conclusion background

---
+++ YouMeetInATavern/Assets/Scripts/Controllers/CloudPuffController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudPuffController : MonoBehaviour {
+
+    public GameObject puffPrefab;
+
+    void OnEnable() {
+        InputController.conclusionBackgroundClicked += HandleConclusionBackgroundClick;
+    }
+
+    void OnDisable() {
+        InputController.conclusionBackgroundClicked -= HandleConclusionBackgroundClick;
+    }
+
+    private void HandleConclusionBackgroundClick(Vector3 position) {
+        GameObject puff = Instantiate(puffPrefab, position, Quaternion.identity, transform);
+        puff.transform.LookAt(Camera.main.transform);
+    }
+}
