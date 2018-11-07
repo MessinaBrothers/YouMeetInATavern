2f151c8b8709eea14290d76fd42ba214b86c5100
Anthony <messinabrothers@gmail.com>
2018-11-07 09:20:39 -0800

    Tested whether checking for mouse input in 10,000 GameObject Update()s would "quickly cause performance problems." See http://answers.unity.com/answers/1410975/view.html Result: no observable effects on performance

---
+++ YouMeetInATavern/Assets/PerformanceCheckMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerformanceCheckMouse : MonoBehaviour {
+
+    public int helperCount;
+
+	void Start () {
+		for (int i = 0; i < helperCount; i++) {
+            GameObject go = new GameObject("Test" + i);
+            go.transform.SetParent(transform);
+            go.AddComponent<PerformanceCheckMouseHelper>();
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ YouMeetInATavern/Assets/PerformanceCheckMouseHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerformanceCheckMouseHelper : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+
+        }
+	}
+}
