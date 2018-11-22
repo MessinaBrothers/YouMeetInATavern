1061e1000bd1ae48c69b31442f9c43e70e5d7237
Anthony <messinabrothers@gmail.com>
2018-11-22 11:03:11 -0800

    GraphUtility parses inquiry questions (i.e. HAS:LOCATION_TOWN produces true if town is selected. MISSING:NPC_BARTENDER produces false if bartender isn't chosen)

---
+++ YouMeetInATavern/Assets/Scripts/Utility/GraphUtility.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphUtility {
+
+    public static bool DoesInquiryPass(GameData data, string inquiryKey) {
+
+        Dialogue inquiry = data.key_dialoguesNEW[inquiryKey];
+        string commands = inquiry.text;
+
+        foreach (string command in commands.Split(' ')) {
+            string[] commandSplit = command.Split(':');
+
+            // add location to answers
+            List<string> chosenAnswers = new List<string>(data.chosenAnswerKeys) {
+                data.chosenLocation.ToString()
+            };
+
+            switch (commandSplit[0]) {
+                case "MISSING":
+                    if (chosenAnswers.Contains(commandSplit[1])) {
+                        return false;
+                    }
+                    break;
+                case "HAS":
+                    if (chosenAnswers.Contains(commandSplit[1]) == false) {
+                        return false;
+                    }
+                    break;
+                default:
+                    Debug.Log("ERROR: cannot parse the command: " + command);
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
